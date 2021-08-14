a54fa7baa0c26a10de37b965d8e357cb3737927b
hodlfantom <88129802+hodlfantom@users.noreply.github.com>
2021-08-14 22:09:14 +0530

    Update WinnerCard.tsx

--- components/WinnerCard.tsx
+++ components/WinnerCard.tsx
@@ -1,1 +1,23 @@
+import tw from "twin.macro"
 
+function WinnerCard({ address, amount, price }) {
+    let add = address.substring(0, 4) + "..." + address.substring(address.length - 4)
+    let url = "https://ftmscan.com/address/" + address
+    let amt = amount.toString().substring(0, 5)
+    let usd = price * amount
+    // console.log(address.substring(1, 2))
+    return (
+        <div tw="flex items-center  text-2xl justify-between w-full">
+            <a href={url} target="_blank">
+                <span>{add}</span>
+            </a>
+            <div tw="flex items-center min-width[60px]">
+                <span tw=" w-full">
+                    ${parseInt(usd.toString())}
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default WinnerCard
